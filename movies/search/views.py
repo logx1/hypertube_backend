c8d87128e0376b5eecdcdbb0db12db616ca1448a
Abdelmajid El-oualy <abdel-ou@c1r5p2.1337.ma>
2026-04-02 20:37:55 +0100

    add simple search

--- movies/search/views.py
+++ movies/search/views.py
@@ -1,11 +1,63 @@
-from django.shortcuts import render
-
-
+import requests
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+import os
 
-# Create your views here.
+def sort_key(movie):
+    try:
+        return int(movie['production_year'])
+    except ValueError:
+        return -1
+
 @api_view(['GET'])
 def search(request):
     query = request.GET.get('q', '')
-    return Response({"results": f"Search results "})
+    
+    if not query:
+        # Based on your project rules: if no search is done, return most popular videos.
+        return Response({"message": "No query provided. You should return popular movies here!"})
+
+    api_key = os.environ.get('tmdb_api_key')
+    
+    # TMDb Search Endpoint
+    url = "https://api.themoviedb.org/3/search/movie"
+    params = {
+        "api_key": api_key,
+        "query": query,
+        "language": "en-US", 
+        "page": 1,           
+        "include_adult": "false"
+    }
+    
+    try:
+        # Make the request to TMDb
+        response = requests.get(url, params=params)
+        response.raise_for_status() 
+        data = response.json()
+        
+        movies = []
+        if "results" in data:
+            for item in data["results"]:
+                poster_path = item.get("poster_path")
+                cover_image = f"https://image.tmdb.org/t/p/w500{poster_path}" if poster_path else None
+                
+
+                release_date = item.get("release_date", "")
+                production_year = release_date.split("-")[0] if release_date else "N/A"
+                
+
+                movies.append({
+                    "name": item.get("title"),
+                    "production_year": production_year,
+                    "rating": item.get("vote_average"), 
+                    "cover_image": cover_image
+                })
+        
+
+        # movies = sorted(movies, key=lambda x: x['name'])
+        movies = sorted(movies, key=sort_key, reverse=True)
+        
+        return Response({"results": movies})
+        
+    except requests.exceptions.RequestException as e:
+        return Response({"error": "Failed to communicate with TMDb.", "details": str(e)}, status=500)
