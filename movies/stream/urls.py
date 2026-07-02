3b1db1f7a4daf175d7de1e092075b43d6a1e65d2
Abdelmajid El-oualy <abdel-ou@c2r7p10.1337.ma>
2026-07-02 18:14:08 +0100

    change urls

--- movies/stream/urls.py
+++ movies/stream/urls.py
@@ -10,7 +10,7 @@ from .views import stream_movie
 
 
 urlpatterns = [
-    path('download',search_movies,name='search'),
+    path('torrent_search',search_movies,name='search'),
     path('downloadx', download_movie, name="start_movie_download"),
     path('watch', stream_movie)
 ]
