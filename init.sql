8be14e9ddff1a1c8cce583faf54c366b29ee2b66
Abdelmajid El-oualy <abdel-ou@c2r7p10.1337.ma>
2026-07-03 12:46:02 +0100

    create new user for my backend

---
+++ init.sql
@@ -0,0 +1,22 @@
+-- Create a new user for the movies backend
+CREATE USER movies_user WITH PASSWORD 'movies_password_here';
+
+-- Create a separate database for the movies backend
+CREATE DATABASE movies_production;
+
+-- Grant all privileges on the database itself
+GRANT ALL PRIVILEGES ON DATABASE movies_production TO movies_user;
+
+-- Connect to the newly created database to apply schema changes
+\c movies_production
+
+-- Grant permission to create things inside the 'public' schema
+GRANT USAGE, CREATE ON SCHEMA public TO movies_user;
+
+-- Optional but recommended: Make movies_user the owner of the public schema
+ALTER SCHEMA public OWNER TO movies_user;
+
+
+
+
+
--- movies/movies/settings.py
+++ movies/movies/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -81,11 +81,14 @@ CORS_ALLOWED_ORIGINS = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_BACKEND_NAME'),
+        'USER': os.environ.get('DB_BACKEND_USER'),
+        'PASSWORD': os.environ.get('DB_MOVIES_BACKEND_PASSWORD'),
+        'HOST': 'postgres',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
